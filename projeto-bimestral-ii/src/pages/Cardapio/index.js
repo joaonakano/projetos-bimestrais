76a97915e684e190028a3776f6bfdcad7b07f90b
BorisPhilipp <engs-borisluz@camporeal.edu.br>
2024-05-30 17:27:54 -0300

    aba cardapio

--- projeto-bimestral-ii/src/pages/Cardapio/index.js
+++ projeto-bimestral-ii/src/pages/Cardapio/index.js
@@ -1,7 +1,58 @@
+import './style.css'
 export default function Cardapio() {
     return (
-        <div style={{color: 'red'}}>
-            <h1>Cardapio</h1>
+        <div className='sa'>
+            <div className="containerCardapio">
+                    <div className="cardapio">
+                        <img src="https://st4.depositphotos.com/13349494/25429/i/450/depositphotos_254299320-stock-photo-selective-focus-delicious-meat-burgers.jpg"/>
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/1a/da/91/29/a-hamburgueria-mais-irreverent.jpg"/>
+                        <img src="https://st4.depositphotos.com/13349494/25429/i/450/depositphotos_254299320-stock-photo-selective-focus-delicious-meat-burgers.jpg"/>
+                    </div>
+                    <div className='preco'>
+                        <p>X-Bosta</p>
+                        <p>McFuckYou</p>
+                        <p>KYS</p>
+                    </div>
+                    <div className='precoReal'>
+                        <p>R$ 25</p>
+                        <p>R$ 98</p>
+                        <p>R$ 777</p>
+                    </div>
+            </div>
+            <div className="containerCardapio">
+                    <div className="cardapio">
+                        <img src="https://st4.depositphotos.com/13349494/25429/i/450/depositphotos_254299320-stock-photo-selective-focus-delicious-meat-burgers.jpg"/>
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/1a/da/91/29/a-hamburgueria-mais-irreverent.jpg"/>
+                        <img src="https://st4.depositphotos.com/13349494/25429/i/450/depositphotos_254299320-stock-photo-selective-focus-delicious-meat-burgers.jpg"/>
+                    </div>
+                    <div className='preco'>
+                        <p>X-Bosta</p>
+                        <p>McFuckYou</p>
+                        <p>KYS</p>
+                    </div>
+                    <div className='precoReal'>
+                        <p>R$ 25</p>
+                        <p>R$ 98</p>
+                        <p>R$ 777</p>
+                    </div>
+            </div>
+            <div className="containerCardapio">
+                    <div className="cardapio">
+                        <img src="https://st4.depositphotos.com/13349494/25429/i/450/depositphotos_254299320-stock-photo-selective-focus-delicious-meat-burgers.jpg"/>
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/1a/da/91/29/a-hamburgueria-mais-irreverent.jpg"/>
+                        <img src="https://st4.depositphotos.com/13349494/25429/i/450/depositphotos_254299320-stock-photo-selective-focus-delicious-meat-burgers.jpg"/>
+                    </div>
+                    <div className='preco'>
+                        <p>X-Bosta</p>
+                        <p>McFuckYou</p>
+                        <p>KYS</p>
+                    </div>
+                    <div className='precoReal'>
+                        <p>R$ 25</p>
+                        <p>R$ 98</p>
+                        <p>R$ 777</p>
+                    </div>
+            </div>
         </div>
     );
 }
