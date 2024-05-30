bd2099b7509ef8c2198b49d5fbe027466965e224
BorisPhilipp <engs-borisluz@camporeal.edu.br>
2024-05-30 18:16:57 -0300

    aba Sobre

--- projeto-bimestral-ii/src/pages/Sobre/index.js
+++ projeto-bimestral-ii/src/pages/Sobre/index.js
@@ -1,7 +1,26 @@
+import './style.css'
 export default function Sobre() {
     return (
-        <div style={{color: 'blue'}}>
-            <h1>Sobre</h1>
+        <div>
+            <div className="sobreContainer">
+                <div className="sobreTexto">
+                    <img src="https://www.shoppingibirapuera.com.br/wp-content/uploads/2021/12/BURGUER-KING.jpeg"/>
+                    <img src="https://ogimg.infoglobo.com.br/economia/23359090-647-bbe/FT1086A/35288084_JE-Rio-de-Janeiro-RJ-24-10-2011-Fachada-de-uma-loja-do-Burger-King-no-RioFoto-Divulgacao.jpg"/>
+                    <h2>Sobre Nós</h2>
+                    <h3>Bem-vindo ao Burger King!</h3>
+                    <p>Desde a inauguração do nosso primeiro restaurante em 1954, o Burger King tem se dedicado a oferecer aos nossos clientes uma experiência de refeição rápida e saborosa. Com mais de 60 anos de tradição, somos reconhecidos mundialmente por nossos hambúrgueres grelhados no fogo, um método que confere aquele sabor inconfundível que você adora.</p>
+                    <br/>
+                    <h3>Nossa Missão</h3>
+                    <p>Nossa missão é simples: servir alimentos de alta qualidade, com rapidez e a um preço acessível, sempre mantendo o compromisso com a excelência e a satisfação dos nossos clientes. Acreditamos que a comida deve ser divertida e deliciosa, por isso nos empenhamos em inovar constantemente, trazendo novos sabores e opções para o nosso cardápio.</p>
+                    <br/>
+                    <h3>Nossa Equipe</h3>
+                    <p>Nossa equipe é composta por indivíduos dedicados e apaixonados pelo que fazem. Cada membro do nosso time é fundamental para o nosso sucesso, e investimos continuamente no desenvolvimento e bem-estar dos nossos funcionários, garantindo um ambiente de trabalho inclusivo e motivador.</p>
+                </div>
+            </div>
+
+
+
+
         </div>
     );
 }
