48fe0f6228f8ad0017153e3563ab8d064f666b37
BorisPhilipp <engs-borisluz@camporeal.edu.br>
2024-05-30 18:39:22 -0300

    aba contato

--- projeto-bimestral-ii/src/pages/Contato/index.js
+++ projeto-bimestral-ii/src/pages/Contato/index.js
@@ -1,7 +1,19 @@
+import './style.css'
 export default function Contato() {
     return (
-        <div style={{color: 'green'}}>
-            <h1>Contato</h1>
+        <div>
+            <div className='container'>
+                <h1>Deseja falar conosco?</h1>
+                <h2>E-mail:</h2>
+                <p>burgerking@king.com</p>
+                <h2>Telefone</h2>
+                <p>(897) 8946-6549</p>
+
+                <h1>Nossas Redes Sociais</h1>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/5/53/X_logo_2023_original.svg'/>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png'/>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg'/>
+            </div>
         </div>
     );
 }
