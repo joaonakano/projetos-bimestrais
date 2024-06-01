a1226a7209ab48af67ff84f3a0d4d564430c3f60
BorisPhilipp <engs-borisluz@camporeal.edu.br>
2024-06-01 20:13:47 -0300

    alteracao na aba contato

--- projeto-bimestral-ii/src/pages/Contato/index.js
+++ projeto-bimestral-ii/src/pages/Contato/index.js
@@ -3,16 +3,24 @@ export default function Contato() {
     return (
         <div>
             <div className='container'>
-                <h1>Deseja falar conosco?</h1>
-                <h2>E-mail:</h2>
-                <p>burgerking@king.com</p>
-                <h2>Telefone</h2>
-                <p>(897) 8946-6549</p>
+            <h2>Deseja nos contatar?</h2>
+                <form/>
+                    <label for="name">Nome:</label>
+                    <input type="text" id="name" name="name"/>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email"/>
+                    <label for="message">Sua Mensagem:</label>
+                    <textarea id="message" name="message"></textarea>
+                    <br/><br/>
+                    <input type="submit" value="Enviar"/>
 
-                <h1>Nossas Redes Sociais</h1>
+                <br/><br/>
+                <h3>Siga-Nos!</h3>
+                <br/>
                 <img src='https://upload.wikimedia.org/wikipedia/commons/5/53/X_logo_2023_original.svg'/>
                 <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png'/>
                 <img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg'/>
+
             </div>
         </div>
     );
