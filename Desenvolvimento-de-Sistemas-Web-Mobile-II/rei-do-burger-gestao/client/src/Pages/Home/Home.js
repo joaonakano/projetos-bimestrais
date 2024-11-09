9cc8252662e4b67e87397214ce536d6c56685f14
João Nakano <138939556+joaonakano@users.noreply.github.com>
2024-11-09 15:25:48 -0300

    data

--- Desenvolvimento-de-Sistemas-Web-Mobile-II/rei-do-burger-gestao/client/src/Pages/Home/Home.js
+++ Desenvolvimento-de-Sistemas-Web-Mobile-II/rei-do-burger-gestao/client/src/Pages/Home/Home.js
@@ -14,7 +14,27 @@ export default function Home() {
     return(
     <>
         <h1>Pedidos</h1>    
-    
+        
+        <ul>
+                {ordersList.map((order) => {
+                    const { documentID, data } = order;
+                    return (
+                        <li key={documentID}>
+                            <h2>Cliente: {data.cliente}</h2>
+                            <p>Valor: R$ {data.valor}</p>
+                            <p>Status: {data.status}</p>
+                            <p>Meio de Pagamento: {data.meioPagamento}</p>
+                            <p>Delivery: {data.delivery ? 'Sim' : 'Não'}</p>
+                            <h3>Pedido:</h3>
+                            <ul>
+                                {data.pedido && data.pedido.map((item, index) => (
+                                    <li key={index}>{item}</li>
+                                ))}
+                            </ul>
+                        </li>
+                    );
+                })}
+            </ul>
     </>
     )
 }
