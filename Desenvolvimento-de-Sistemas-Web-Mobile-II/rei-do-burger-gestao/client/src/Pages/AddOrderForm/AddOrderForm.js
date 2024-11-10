293399eb63c7c82aa7230cef983bfdef46d75003
Boris Philipp <engs-borisluz@camporeal.edu.br>
2024-11-10 00:03:59 -0300

    estilizacao da pagina de add

--- Desenvolvimento-de-Sistemas-Web-Mobile-II/rei-do-burger-gestao/client/src/Pages/AddOrderForm/AddOrderForm.js
+++ Desenvolvimento-de-Sistemas-Web-Mobile-II/rei-do-burger-gestao/client/src/Pages/AddOrderForm/AddOrderForm.js
@@ -61,137 +61,149 @@ export default function AddOrderForm() {
     
     return(
         <>
-            <h1>Adicionar Pedido</h1>
-
-            <form className="add-form" onSubmit={handleSubmit}>
-                <label>Cliente:</label><br/>
-                <input
-                    type="text"
-                    placeholder="João Silva"
-                    name="client-name"
-                    value={client}
-                    onChange={e => {setClient(e.target.value)}}
-                    required
-                /><br/>
-
-                <label>Delivery:</label><br/>
-                <input
-                    type="radio"
-                    id="nao"
-                    name="delivery-options"
-                    value="nao"
-                    onChange={e => {setDelivery(false)}}
-                    checked={delivery === false}
-                    required
-                    />
-                <label for="nao">Não</label>
-                <input
-                    type="radio"
-                    id="sim"
-                    name="delivery-options"
-                    value="sim"
-                    onChange={e => {setDelivery(true)}}
-                    required
-                />
-                <label for="sim">Sim</label><br/>
-
-                <label>Meio de Pagamento:</label>
-                <select
-                    id="payment-method"
-                    name="payment-method"
-                    value={paymentMethod}
-                    onChange={e => {setPaymentMethod(e.target.value)}}
-                >
-                    <option
-                        value="cartao-credito"
-                    >Crédito
-                    </option>
-
-                    <option
-                        value="cartao-debito"
-                        >Débito
-                    </option>
-
-                    <option
-                        value="dinheiro"
-                        checked={paymentMethod === "dinheiro"}
-                    >Dinheiro
-                    </option>
-                    
-                    <option
-                        value="pix"
-                    >PIX</option>
-                </select><br/>
-
-                <div>
-                    <label>Status:</label><br/>
-                    <label> {/* Coloquei o LABEL acima do INPUT para o usuario selecionar a opcao clicando no texto, nao somente na bolinha, se aparecer algum conflito, considere mover o LABEL para BAIXO do INPUT */}
+            <div className="container-Add">
+                <h1>Adicionar Pedido</h1>
+                <div className="grade-Add">
+                    <form className="add-form" onSubmit={handleSubmit}>
+                        <label>Cliente:</label><br/>
+                        <input
+                            type="text"
+                            placeholder="João Silva"
+                            name="client-name"
+                            value={client}
+                            onChange={e => {setClient(e.target.value)}}
+                            required
+                        /><br/>
+                        <br/>
+                        <label>Delivery:</label><br/>
                         <input
                             type="radio"
-                            id="fazer"
-                            name="status-options"
-                            value="fazer"
-                            onChange={e => {setStatus(e.target.value)}}
-                            checked={status === "fazer"}
+                            id="nao"
+                            name="delivery-options"
+                            value="nao"
+                            onChange={e => {setDelivery(false)}}
+                            checked={delivery === false}
                             required
                             />
-                        A Fazer
-                    </label>
-
-                    <label>
-                        <input 
-                            type="radio"
-                            id="andamento"
-                            name="status-options"
-                            value="andamento"
-                            onChange={e => {setStatus(e.target.value)}}
-                            checked={status === "andamento"}
-                            required
-                            />
-                        Em Andamento
-                    </label>
-
-                    <label>
+                        <label for="nao"> Não</label>
                         <input
                             type="radio"
-                            id="concluido"
-                            name="status-options"
-                            value="concluido"
-                            onChange={e => {setStatus(e.target.value)}}
-                            checked={status === "concluido"}
+                            id="sim"
+                            name="delivery-options"
+                            value="sim"
+                            onChange={e => {setDelivery(true)}}
                             required
                         />
-                        Concluído
-                    </label><br/>
+                        <label for="sim"> Sim</label><br/>
+
+                        <br/>
+                        <label>Meio de Pagamento:</label>
+                        <select
+                            id="payment-method"
+                            name="payment-method"
+                            value={paymentMethod}
+                            onChange={e => {setPaymentMethod(e.target.value)}}
+                        >
+                            <option
+                                value="cartao-credito"
+                            >Crédito
+                            </option>
+
+                            <option
+                                value="cartao-debito"
+                                >Débito
+                            </option>
+
+                            <option
+                                value="dinheiro"
+                                checked={paymentMethod === "dinheiro"}
+                            >Dinheiro
+                            </option>
+
+                            <option
+                                value="pix"
+                            >PIX</option>
+                        </select><br/>
+                        <br/>
+                        <div>
+                            <label>Status:</label><br/>
+                            <label> {/* Coloquei o LABEL acima do INPUT para o usuario selecionar a opcao clicando no texto, nao somente na bolinha, se aparecer algum conflito, considere mover o LABEL para BAIXO do INPUT */}
+                                <input
+                                    type="radio"
+                                    id="fazer"
+                                    name="status-options"
+                                    value="fazer"
+                                    onChange={e => {setStatus(e.target.value)}}
+                                    checked={status === "fazer"}
+                                    required
+                                    />
+                                A Fazer
+                            </label>
+
+                            <label>
+                                <input 
+                                    type="radio"
+                                    id="andamento"
+                                    name="status-options"
+                                    value="andamento"
+                                    onChange={e => {setStatus(e.target.value)}}
+                                    checked={status === "andamento"}
+                                    required
+                                    />
+                                Em Andamento
+                            </label>
+
+                            <label>
+                                <input
+                                    type="radio"
+                                    id="concluido"
+                                    name="status-options"
+                                    value="concluido"
+                                    onChange={e => {setStatus(e.target.value)}}
+                                    checked={status === "concluido"}
+                                    required
+                                />
+                                Concluído
+                            </label><br/>
+                        </div>
+                        <br/>
+
+                        <label>Pedido:</label><br/>
+                        {
+                            orders.map(order => (
+                                <label key={order.id}>
+                                    <input
+                                        type="checkbox"
+                                        onChange={() => {handleOrderChange(order.name)}}
+                                    />
+                                    {order.name}
+                                </label>
+                            ))
+                        }<br/>
+                        <br/>
+
+                        <label>Total:</label><br/>
+                        <input
+                            type="number"
+                            placeholder="24.50"
+                            min="0"
+                            step="0.01"
+                            name="total"
+                            value={total}
+                            onChange={e => {setTotal(e.target.value * 1)}}
+                            required
+                        /><br/>
+                       
+                       <br/>
+                        <button type="submit">Registrar Pedido</button>
+                    </form>
                 </div>
 
-                <label>Pedido:</label><br/>
-                {
-                    orders.map(order => (
-                        <label key={order.id}>
-                            <input
-                                type="checkbox"
-                                onChange={() => {handleOrderChange(order.name)}}
-                            />
-                            {order.name}
-                        </label>
-                    ))
-                }<br/>
-               
-                <label>Total:</label><br/>
-                <input
-                    type="number"
-                    placeholder="24.50"
-                    min="0"
-                    step="0.01"
-                    name="total"
-                    value={total}
-                    onChange={e => {setTotal(e.target.value * 1)}}
-                    required
-                /><br/>
+                <footer>
+                © 2024 - Todos os direitos reservados.
+                </footer>
 
-                <button type="submit">Registrar Pedido</button>
-            </form>
+            </div>
         </>
     )
 }
