423b959fb964bcfcfa7963504d2a2aea48d1fab6
Emanuelprime <emanuelll3000@gmail.com>
2024-11-11 10:19:41 -0300

    att-style teste

--- Desenvolvimento-de-Sistemas-Web-Mobile-II/rei-do-burger-gestao/client/src/Components/Header/Header.js
+++ Desenvolvimento-de-Sistemas-Web-Mobile-II/rei-do-burger-gestao/client/src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import "./style.css"
+import "./style.css";
 
 export default function Header() {
     return(
@@ -6,13 +6,10 @@ export default function Header() {
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet" />
-
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
 
             <header>
-            <div className="nav-bar">
+                <div className="nav-bar">
                     <span id="brand-name">Burger King</span>
                     <div id="nav-items">
                         <a href="/add">Adicionar um Pedido</a>
