7c659d768c2c350850e02c7f0b439abd705e8e8a
Emanuelprime <emanuelll3000@gmail.com>
2024-11-11 10:28:34 -0300

    style home

--- Desenvolvimento-de-Sistemas-Web-Mobile-II/rei-do-burger-gestao/client/src/Pages/Home/Home.js
+++ Desenvolvimento-de-Sistemas-Web-Mobile-II/rei-do-burger-gestao/client/src/Pages/Home/Home.js
@@ -1,74 +1,69 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import RemoveButton from '../../Components/Button/Remove/Remove'
-import UpdateButton from '../../Components/Button/Update/Update'
-import Header from "../../Components/Header/Header"
-import "./style.css"
+import RemoveButton from '../../Components/Button/Remove/Remove';
+import UpdateButton from '../../Components/Button/Update/Update';
+import Header from "../../Components/Header/Header";
+import "./style.css";
 
 export default function Home() {
-    const [ordersList, setOrdersList] = useState([])
-    
+    const [ordersList, setOrdersList] = useState([]);
+
     useEffect(() => {
         axios.get('http://localhost:8000/api/get/all')
             .then((res) => {
-                setOrdersList(res.data)
-            })
-    }, [])
-    
+                setOrdersList(res.data);
+            });
+    }, []);
+
     useEffect(() => {
         axios.get('http://localhost:8000/api/get/all')
             .then((res) => {
-                setOrdersList(res.data)
-        })
-    }, [ordersList])
+                setOrdersList(res.data);
+            });
+    }, [ordersList]);
 
     function updateOrdersList(newListData) {
-        setOrdersList(newListData)
+        setOrdersList(newListData);
     }
-    
+
     function getOrdersList() {
-        return ordersList
+        return ordersList;
     }
 
-    return(
-    <>
-    <div className='container-pedidos'>
-              
+    return (
+        <>
             <Header />
-
-            <h1>Pedidos</h1>
-            <div className='grade-pedidos'>
+            <div className='container-pedidos'>
+                <h1>Pedidos</h1>
+                <div className='grade-pedidos'>
                     {ordersList.map((order) => {
                         const { documentID, data } = order;
                         return (
-
                             <div key={documentID} className='pedido-item'>
-                                <RemoveButton id={documentID} getOrdersList={getOrdersList} updateOrdersList={updateOrdersList}/>
-                                <UpdateButton id={documentID}/>
+                                <RemoveButton id={documentID} getOrdersList={getOrdersList} updateOrdersList={updateOrdersList} />
+                                <UpdateButton id={documentID} />
                                 <h2 className="header-card">{data.cliente}</h2>
+                                <hr />
                                 <p>Valor: R${data.valor}</p>
                                 <p>Status: {data.status}</p>
                                 <p>Meio de Pagamento: {data.meioPagamento}</p>
                                 <p>Delivery: {data.delivery ? 'Sim' : 'Não'}</p>
+                                <hr />
                                 <h3>Pedido:</h3>
                                 <ul>
                                     {data.pedido && data.pedido.map((item, index) => (
                                         <li key={index}>{item}</li>
                                     ))}
                                 </ul>
-
                             </div>
-
                         );
                     })}
+                </div>
+                <footer>
+                    © Rei do Burger 2024 - Todos os direitos reservados.
+                </footer>
             </div>
-
-            <footer>
-                © Rei do Burger 2024 - Todos os direitos reservados.
-            </footer>
-
-    </div>
-    </>
-    )
+        </>
+    );
 }
